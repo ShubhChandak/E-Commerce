188b0794f7716bd0c7f304483746f9bc71d46034
Shubh Chandak <shubhchandak0608@gmail.com>
2021-05-25 12:25:13 +0530

    modifications

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -150,11 +150,9 @@ userRouter.post(
         user.email = req.body.email || user.email;
         ///user.isSeller = req.body.isSeller || user.isSeller;
        /// user.isAdmin = req.body.isAdmin || user.isAdmin;
-       user.isSeller =
-req.body.isSeller === user.isSeller ? user.isSeller : req.body.isSeller;
-
-  user.isAdmin =
-    req.body.isAdmin === user.isAdmin ? user.isAdmin : req.body.isAdmin;
+       user.isSeller = Boolean(req.body.isSeller);
+      user.isAdmin = Boolean(req.body.isAdmin);
+      // user.isAdmin = req.body.isAdmin || user.isAdmin;
       const updatedUser = await user.save();
       res.send({ message: 'User Updated', user: updatedUser });
     } else {
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -78,7 +78,7 @@ export const signin = (email, password) => async (dispatch) => {
     } = getState();
     try {
       const { data } = await Axios.get(`/api/users/${userId}`, {
-        headers: { Authorization: `Bearer ${userInfo.token}` },
+        headers: { Authorization: `Bearer ${userInfo?.token}` },
       });
       dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
     } catch (error) {
